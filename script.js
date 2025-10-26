6272abeb6b84a760e9e2aa631b8812bd1c1e9dca
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2025-10-26 01:10:09 +0100

    overflowing scroll reveal fixed

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ ScrollReveal().reveal(".about-left", {
 
 ScrollReveal().reveal(".about-right", {
   origin: "right",
-  distance: "40px",
+  distance: "15px",
   duration: 800,
   easing: "ease-in-out",
   opacity: 0,
